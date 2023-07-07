003927379885e527a18eb846c36cd3a25c777a0d
printer83mph <printer.83mph@gmail.com>
2023-07-07 13:00:15 -0400

    Switch content to submodule

--- src/markdown/lectures/mdx.ts
+++
@@ -1,9 +0,0 @@
-import { default as lec00 } from './0-course-policies.mdx';
-
-import { LectureSlug } from '@/constants/lectures';
-
-export const lectureMdx: { [key in LectureSlug]: (props: any) => JSX.Element } =
-  {
-    '0-course-policies': lec00,
-    '1-js-basics': lec00,
-  };
