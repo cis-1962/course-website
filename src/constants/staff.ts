63b62e77724c33cbea933a44a795ff74735336db
marcel kida <47465692+marcelkida@users.noreply.github.com>
2023-08-30 20:22:38 -0400

    added marcel to staff page

--- src/constants/staff.ts
+++ src/constants/staff.ts
@@ -25,4 +25,12 @@ export const STAFF = [
       'Hi, I’m Kashyap, an undergrad junior studying CS @ Penn. I’m involved in Penn Spark, a club focused on design and engineering projects; If you’re interested in building something fun please check us out!',
     image: 'kashyap.png',
   },
+  {
+    name: 'Marcel Kida',
+    role: 'TA',
+    email: 'mkida@wharton.upenn.edu',
+    description:
+      "I'm a junior in M&T. I like playing volleyball and rock climbing. Let's talk anything about design, fashion, or tech!",
+    image: 'marcel.jpeg',
+  },
 ] as const;
