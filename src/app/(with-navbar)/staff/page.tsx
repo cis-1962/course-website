ea00baf25259649139a431baff35d82b6849e540
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-09-02 02:14:59 -0400

    feat: add oh locations

--- src/app/(with-navbar)/staff/page.tsx
+++ src/app/(with-navbar)/staff/page.tsx
@@ -13,39 +13,37 @@ export const metadata = {
 export default function StaffPage() {
   return (
     <main className="grid grid-flow-row grid-cols-1 gap-x-8 gap-y-12 md:grid-cols-2">
-      {STAFF.map(
-        ({ name, role, email, oh: officeHours, description, image }) => (
-          <figure key={email}>
-            <div className="relative aspect-square overflow-hidden rounded-3xl">
-              <Image
-                alt={name}
-                src={`${STAFF_IMAGE_FOLDER}/${image}`}
-                fill
-                className="object-cover"
-              />
+      {STAFF.map(({ name, role, email, oh, description, image }) => (
+        <figure key={email}>
+          <div className="relative aspect-square overflow-hidden rounded-3xl">
+            <Image
+              alt={name}
+              src={`${STAFF_IMAGE_FOLDER}/${image}`}
+              fill
+              className="object-cover"
+            />
+          </div>
+          <figcaption>
+            <h2 className="mt-4 font-display text-3xl font-bold tracking-[-0.01em] dark:font-semibold">
+              {name}
+            </h2>
+            <div className="font-medium opacity-50">{role}</div>
+            <h4 className="mt-2">
+              OH: <b>{`${oh.time}, ${oh.location}`}</b>
+            </h4>
+            <p className="mt-2 tracking-[0.01em]">{description}</p>
+            <div className="mt-1 flex items-center gap-1">
+              <a
+                href={`mailto:${email}`}
+                className="font-medium underline hover:opacity-80 dark:font-normal"
+              >
+                {email}
+              </a>
+              <EmailCopyButton email={email} />
             </div>
-            <figcaption>
-              <h2 className="mt-4 font-display text-3xl font-bold tracking-[-0.01em] dark:font-semibold">
-                {name}
-              </h2>
-              <div className="font-medium opacity-50">{role}</div>
-              <h4 className="mt-2">
-                Office Hours: <b>{officeHours}</b>
-              </h4>
-              <p className="mt-2 tracking-[0.01em]">{description}</p>
-              <div className="mt-1 flex items-center gap-1">
-                <a
-                  href={`mailto:${email}`}
-                  className="font-medium underline hover:opacity-80 dark:font-normal"
-                >
-                  {email}
-                </a>
-                <EmailCopyButton email={email} />
-              </div>
-            </figcaption>
-          </figure>
-        )
-      )}
+          </figcaption>
+        </figure>
+      ))}
     </main>
   );
 }
--- src/constants/staff.ts
+++ src/constants/staff.ts
@@ -5,7 +5,10 @@ export const STAFF = [
     name: 'Thomas Shaw',
     role: 'Lecturer',
     email: 'tlshaw@sas.upenn.edu',
-    oh: 'Wednesday 8-10pm',
+    oh: {
+      location: '3rd Floor Levine Bump Space',
+      time: 'Wednesday 8-10pm',
+    },
     description:
       "Hi! I'm Thomas, an undergrad junior in the DMD program at Penn. I also help run the UPenn SIGGRAPH club, which you should definitely check out if you have any interest in graphics tech!",
     image: 'thomas.png',
@@ -14,7 +17,10 @@ export const STAFF = [
     name: 'Tuneer Roy',
     role: 'Lecturer',
     email: 'tuneer@seas.upenn.edu',
-    oh: 'Thursday 3:30-5pm',
+    oh: {
+      location: '3rd Floor Levine Bump Space',
+      time: 'Thursday 3:30-5pm',
+    },
     description:
       "Hey! I'm Tuneer, a junior studying CS at Penn. I'm also a Team Lead for Penn Mobile @ Penn Labs, so if the app ever breaks... my bad.",
     image: 'tuneer.png',
@@ -23,7 +29,10 @@ export const STAFF = [
     name: 'Kashyap Chaturvedula',
     role: 'TA',
     email: 'kashyapc@seas.upenn.edu',
-    oh: 'Tuesday 5:15-6:45',
+    oh: {
+      location: '3rd Floor Levine Bump Space',
+      time: 'Tuesday 5:15-6:45',
+    },
     description:
       'Hi, I’m Kashyap, an undergrad junior studying CS @ Penn. I’m involved in Penn Spark, a club focused on design and engineering projects; If you’re interested in building something fun please check us out!',
     image: 'kashyap.png',
@@ -32,7 +41,10 @@ export const STAFF = [
     name: 'Marcel Kida',
     role: 'TA',
     email: 'mkida@wharton.upenn.edu',
-    oh: 'Monday 6:30-7:30pm',
+    oh: {
+      location: '3rd Floor Levine Bump Space',
+      time: 'Monday 6:30-7:30pm',
+    },
     description:
       "I'm a junior in M&T. I like playing volleyball and rock climbing. Let's talk anything about design, fashion, or tech!",
     image: 'marcel.jpeg',
